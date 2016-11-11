484bb3c448069cf359fbb5f9f7ea5ac29b3677b2
b215053 <shubhamsinghoriginal@gmail.com>
2016-11-11 15:16:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -89,11 +89,7 @@ function loadLogin () {
     request.send(null);
 }
 
-function hello()
-{
-    alert('hello');
-}
-hello();
+
 // The first thing to do is to check if the user is logged in!
 loadLogin();
 

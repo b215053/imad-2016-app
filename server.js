f7343154538b60f1d94a6951b755e8906b75384a
b215053 <shubhamsinghoriginal@gmail.com>
2016-11-12 00:20:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -51,14 +51,15 @@ function createTemplate (data) {
 </div>
 <div id="content">
 ${content}
-</div>
-
 <div id="comment_form">
     
 </div>
 <div id="comments">
     
 </div>
+</div>
+
+
 <script type="text/javascript" src="/ui/article.js"></script>
 <script type="text/javascript" src="/ui/article.js"></script>
 </body>
